69de73cd83a044a54e94bfe0847af1f9f304b6cc
DIvanoski547 <144272402+DIvanoski547@users.noreply.github.com>
2024-04-10 12:30:49 +0200

    still needs work

---
+++ profile-app-server/models/User.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+    },
+    campus: [
+      'Madrid',
+      'Barcelona',
+      'Miami',
+      'Paris',
+      'Berlin',
+      'Amsterdam',
+      'México',
+      'Sao Paulo',
+      'Lisbon',
+      'Remote',
+    ],
+
+    course: ['Web Dev', 'UX/UI', 'Data Analytics', 'Cyber Security'],
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
